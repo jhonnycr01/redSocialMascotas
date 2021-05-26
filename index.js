fddaba628f7fff3dfa47bc1039381451ff597819
jhonny <jhonny.catano1@correo.icesi.edu.co>
2021-05-26 09:10:53 -0500

    v4

--- index.js
+++ index.js
@@ -1,42 +1,91 @@
+const dataBase = firebase.firestore();
 
-var nombresD = [];
-var fechasD = [];
-var telefonosD = [];
+class Dueño{
+    constructor(nombreD , fechaD, telefonoD){
+        this.nombreD = nombreD;
+        this.fechaD = fechaD;
+        this.telefonoD = telefonoD;
+    }
+
+    getDatos(){
+        return("nombre y apellidos: " + this.nombreD + "fecha nacimiento " + this.fechaD +
+        " telefono " + this.telefonoD);
+    }
+    getNombre(){
+        return(this.nombreD);
+    }
+    getFecha(){
+        return(this.fechaD);
+    }
+    getTelefono(){
+        return(this.telefonoD);
+    }
+
+     async guardarDueño(){
+        await dataBase.collection('duenos').doc().set({ 
+            nombreD : this.nombreD,
+            fechaD : this.fechaD,
+            telefonoD : this.telefonoD
+        });
+    } 
+}
+
+class UI{
+    añadirDueño(){
+    var nombreD = document.getElementById("nombreD").value;
+    var fechaD = document.getElementById("fechaD").value;
+    var telefonoD = document.getElementById("telefonoD").value;
+
+    if(this.valUsuario(nombreD)){
+        alert("dueño ya existe");
+    }else{
+        const dueño = new Dueño(nombreD, fechaD, telefonoD);     
+            dueño.guardarDueño();
+    }
+    }
+
+async valUsuario(nombreD){
+    const querySnapshot = await dbApp.collection('duenos').get();
+    var existe = false;
+    querySnapshot.forEach(doc => {
+        if (nombreD == doc.data().name){
+            existe = true;
+        } 
+    });
+    return (existe);
+}
+async cargarDueños(){
+    const querySnapshot = await dbApp.collection('duenos').get();
+    const studentContainer = document.getElementById('student-list');
+    querySnapshot.forEach(doc => {  
+        console.log(doc.data().name);
+        studentContainer.innerHTML += "<div> " + doc.data().name +
+                                      "</div>"; 
+    });
+}
+}
+
+uiApp = new UI();
+window.addEventListener('DOMContentLoaded', function (e){
+    uiApp.cargarDueños();
+})
+const studentForm = document.getElementById('formularioDueños');
+
+studentForm.addEventListener('submit', function(e){
+    e.preventDefault();    
+    uiApp.añadirDueño();        
+})
 
 var botonD = document.getElementById("botonRegistrarD");
 
-botonD.addEventListener('click', guardarDatosD);
+botonD.addEventListener('click', añadirDueño);
 
 var botonContinuar = document.getElementById("botonContinuar");
 
-botonContinuar.addEventListener('click' , condiciones);
+botonContinuar.addEventListener('submit' ,condicion);
 
-
-function guardarDatosD(){
-    var nombreD = document.getElementById("nombreD").value;
-    var fechaD = document.getElementById("fechaD").value;
-    var telefonoD = document.getElementById("telefonoD").value;
-
-    nombresD.push(nombreD);
-    fechasD.push(fechaD);
-    telefonosD.push(telefonoD);
-
-    alert("datos guardados");
-    
-    console.log(nombreD + " " + fechaD + " " + telefonoD);
-}
-
-function condiciones(){
-    var nombreD = document.getElementById("nombreD").value;
-    var fechaD = document.getElementById("fechaD").value;
-    var telefonoD = document.getElementById("telefonoD").value;
-    if(nombresD.indexOf(nombreD)===-1){
-        alert("los datos no han sido ingresados");
-        return;
-    }
-
-        location.href ="inicio.html";
-    
+function condicion(){
+        location.href="inicio.html"
 }
 
 var botonInvitados = document.getElementById("botonInvitado");
--- inicio.js
+++ inicio.js
@@ -1,17 +1,86 @@
-var nombresM = [];
-var tiposM = [];
-var razasM= [];
-var edadesM= [];
-var cumpleañossM= [];
-var gustossM= [];
-var horariosPaseoM= [];
+const dataBase = firebase.firestore();
+
+class Mascota{
+    constructor(nombresM, tiposM, razasM, edadesM, cumpleañossM, gustossM, horariosPaseoM){
+    
+              this.nombresM = nombresM;
+              this.tiposM = tiposM;
+              this.razasM = razasM; 
+              this.edadesM = edadesM;
+              this.cumpleañossM = cumpleañossM;
+              this. gustossM = gustossM;
+              this.horariosPaseoM = horariosPaseoM;
+    }
+    getDatos(){
+        return("Nombre: " + this.nombresM + " \nTipo: " + this.tiposM + "raza" + this.razasM +
+        " \nEdad: " + this.edadesM + " \nCumpleaños: " + this.cumpleañossM + " \nGustos: " + this.gustossM + 
+        " \nHorarios: " + this.horariosPaseoM);
+        }
+        getNombresM(){
+          return (this.nombresM);
+        }
+        
+        getTiposM(){
+          return (this.tiposM);
+        }
+        
+        getRazasM(){
+          return (this.razasM);
+        }
+        getNEdadesM(){
+          return (this.edadesM);
+        }
+        getCumpleañossM(){
+          return (this.cumpleañossM);
+        }
+        getGustossM(){
+          return (this.gustossM);
+        }
+        getHorariosPaseoM(){
+          return (this.horariosPaseoM);
+        }
+        
+    }
+
+    class Evento{
+        constructor(nombresE, fechasE, duracionesE, requisitossE, descripcionesE){
+        
+                  this.nombresE = nombresE;
+                  this.fechasE = fechasE;
+                  this.duracionesE= duracionesE; 
+                  this.requisitossE= requisitossE;
+                  this.descripcionesE = descripcionesE;
+        }
+        getDatos(){
+        return("Nombre: " + this.nombresE + " \nFecha: " + this.fechasE + 
+        " \nDuración: " + this.duracionesE + " \nRequisitos: " + this.requisitossE+ " \nDescripción: " + this.descripcionesE);
+        }
+        getNombresM(){
+          return (this.nombresE);
+        }
+        
+        getFechasE(){
+          return (this.fechasE);
+        }
+        
+        getDuracionesE(){
+          return (this.duracionesE);
+        }
+        getRequisitosE(){
+          return (this.requisitossE);
+        }
+        getDescripcionesE(){
+          return (this.descripcionesE);
+        }
+       
+        }
 
 
-var nombresE =[];
-var fechasE = [];
-var duracionesE = [];
-var requisitossE = [];
-var descripcionesE = []; 
+
+
+
+
+
 
 var botonRegistrarM = document.getElementById("botonRegistrarM");
 
@@ -20,61 +89,6 @@ botonRegistrarM.addEventListener('click', guardarDatosM);
 var botonRegistrarE = document.getElementById("botonRegistrarE");
 
 botonRegistrarE.addEventListener('click', guardarDatosE);
-
-    function guardarDatosM(){
-    var nombreM = document.getElementById("nombreM").value;
-    var tipoM = document.getElementById("tipoM").value;
-    var razaM = document.getElementById("razaM").value;
-    var edadM = document.getElementById("edadM").value;
-    var cumpleañosM = document.getElementById("cumpleañosM").value;
-    var gustosM = document.getElementById("gustosM").value;
-    var horarioPaseoM = document.getElementById("horarioPaseoM").value;
-
-    nombresM.push(nombreM);
-    tiposM.push(tipoM);
-    razasM.push(razaM);
-    edadesM.push(edadM);
-    cumpleañossM.push(cumpleañosM);
-    gustossM.push(cumpleañosM);
-    horariosPaseoM.push(horarioPaseoM);
-
-    alert("datos guardados");
-
-    console.log(nombreM + " " + tipoM + " " + razaM + " " + edadM + " " + cumpleañosM + " " + gustosM + 
-    " " + horarioPaseoM);
-}
-
-
-
- function guardarDatosE(){
-    var nombreE = document.getElementById("nombreE").value;
-    var fechaE = document.getElementById("fechaE").value;
-    var duracionE = document.getElementById("duracionE").value;
-    var requisitosE = document.getElementById("requisitosE").value;
-    var descripcionE = document.getElementById("descripcionE").value;
-
-    nombresE.push(nombreE);
-    fechasE.push(fechaE);
-    duracionesE.push(duracionE);
-    requisitossE.push(requisitosE);
-    descripcionesE.push(descripcionE);
-
-    alert("datos guardados");
-
-    console.log(nombreE + " " + fechaE + " " + duracionE + " " + requisitosE + " " + descripcionE);
-}
-
-var botonCancelarE = document.getElementById("botonCancelarEvento");
-
-botonCancelarE.addEventListener('click', cancelarEvento);
-
-function cancelarEvento(){
-    var nombreE = document.getElementById("nombreE").value;
-    var indexOf = nombresE.indexOf(nombreE);
-    var eliminar = nombresE.splice(indexOf);
-    alert("el evento" + nombreE + "ha sido eliminado");
-
-}
 
 var botonConsultarEvento = document.getElementById("botonConsultarEvento");
 
